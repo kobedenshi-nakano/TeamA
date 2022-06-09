981ca4fc7908ed3167f80970bcf0b6761198d59a
kobedenshi-nakano <kd1253240@st.kobedenshi.ac.jp>
2022-06-09 20:40:26 +0900

    a

--- mainfile/createbun.php
+++ mainfile/createbun.php
@@ -23,7 +23,7 @@ if($_REQUEST["search"]){
 	<div class="main-contents-container">
 		<div class="column1">
 			<ul class="subnav">
-				<li><a href="<?php require_once __DIR__ .'/update.php'; ?>">update</a></li>
+				<li><a href="<?php require .'/update.php'; ?>">update</a></li>
 				<li><a href="select.php">select</a></li>
 				<li><a href="delete.php">delete</a></li>
 				<li><a href="insert.php">insert</a></li>
