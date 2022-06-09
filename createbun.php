d9db92bfa9efc1b7d6d587f5475e7916cc59bbf7
kobedenshi-nakano <kd1253240@st.kobedenshi.ac.jp>
2022-06-09 14:02:43 +0900

    a

--- createbun.php
+++ createbun.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html>
+<?
+if($_REQUEST["search"]){
+//submitされた検索条件を使いDBに問い合わせる処理
+}
+?>
 <html lang="ja">
 <head>
 <meta charset="UTF-8">
@@ -6,7 +11,7 @@
 <meta name="format-detection" content="telephone=no">
 <title>SQLツクール</title>
 <link rel="stylesheet" href="home.css">
-<link rel="stylesheet" href="../css/createbun.css">
+<link rel="stylesheet" href="css/createbun.css">
 </head>
 <body>
 <header>
@@ -17,7 +22,6 @@
 <div class="main-contents">
 	<div class="main-contents-container">
 		<div class="column1">
-			<!--<h1>About</h1>-->
 			<ul class="subnav">
 				<li><a href="#">update</a></li>
 				<li><a href="#">select</a></li>
@@ -25,28 +29,26 @@
 				<li><a href="#">insert</a></li>
 			</ul>
 		</div>
+
 		<div class="column2">
-			<div class="news">
-				<!--<h2>お知らせ</h2>-->
 				<ul class="news-contents">
-					<!--<li>
-						<p>固定席お試しキャンペーン実施中。11月1日〜14日の2週間、固定席をシェアードスペース料金でご利用いただけます。毎日先着2席分です！</p>
-					</li>
-					<li>
-						<p>ご利用者主催のイベントをバックアップします。座卓スペース予約、エリア予約、全体貸切など、大小様々なイベントにご利用ください。</p>
-					</li>
-					<li>
-						<p>ウェブデザイナー／フロントエンドエンジニアの交流勉強会を定期的に開催しています。次回は11月30日、エントリー受付中です。</p>
-					</li>
-					<li>
-						<p>話題のトースターがキッチンスペースに登場しました。いつでも自由にご利用いただけますので、お試しください。</p>
-					</li>-->
+			<form method="post" action="">
+				<input type="text" name="keyword" value="<?=$keyword?>"/>
+				<input type="submit" name="search" value="検索" />
+			</form>
+				<?
+					if($_REQUEST["search"]){
+				?>
+					<!--検索結果を表示する-->
+				<?
+					}
+				?>
 				</ul>
-			</div>
 		</div>
-		
+    </form>
 	</div>
 </div>
+
 <footer>
 	<div class="footer-container">
 		<p class="copyright">©PBL2-Aチーム</p>
