ce8fc834b863060216c4b90d3b8009302bdc653e
kobedenshi-nakano <kd1253240@st.kobedenshi.ac.jp>
2022-10-13 10:13:16 +0900

    検証中

--- mainfile/script01.js
+++ mainfile/script01.js
@@ -2,7 +2,8 @@
 let copy_text =$json_value;
 let copy_text1=$json_value1;
 let copy_btn = document.getElementById('copy-btn');
-  copy_btn.addEventListener(`click`, navigator.clipboard.writeText(copy_text).then(() => {
+  copy_btn.addEventListener(`click`, navigator.clipboard.writeText(copy_text).then(
+    () => {
     // true
     console.log("Copied it to the clipboard.");
    }
