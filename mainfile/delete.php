73296a5e034ab0037c650588ab11ebbcd3af996d
kobedenshi-nakano <kd1253240@st.kobedenshi.ac.jp>
2022-10-14 09:48:44 +0900

    クリップボードの追加

--- mainfile/delete.php
+++ mainfile/delete.php
@@ -245,7 +245,7 @@
 		</div>
 		<button id="btn">Copy text</button>
 		<script>
-			const btn = document.getElementById("btn"); // button要素取得
+		const btn = document.getElementById("btn"); // button要素取得
 		const txt = document.getElementById("copy-text").textContent; // テキスト取得
 
 		btn.addEventListener("click", () => { // ボタンをクリックしたら
