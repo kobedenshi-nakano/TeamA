f57ffe00214d8fce8a6ae3499da08fb68f9c4616
kobedenshi-nakano <kd1253240@st.kobedenshi.ac.jp>
2022-11-29 10:05:57 +0900

    Co-authored-by: kennag0707 <kennag0707@users.noreply.github.com> Co-authored-by: KasaiYuuto <KasaiYuuto@users.noreply.github.com>

--- mainfile/savehistory.php
+++ mainfile/savehistory.php
@@ -1,6 +1,34 @@
 <?php
+	require_once '../classes/dbc.php';
     require_once __DIR__ .'./header.php';
 	/*require_once __DIR__ .'./subnav.php';*/
+	$dbc = new Dbc();
+	$blogdata = $dbc->getAllBlog();
+	try{
+
+		//DBに接続
+		$dsn = 'mysql:host=localhost;dbname=localtest;charset=utf8';
+		$user= 'root';
+		$pass= 'pass';
+	
+		$dbh = new \PDO($dsn,$user,$pass,[
+			\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+			]);
+	
+		$blog = $_POST;
+		$sql ="select * from memory" ;
+		$sth = $dbh->prepare($sql);
+		$sth->execute();
+		$result = $sth->fetchAll();
+	
+		$goal=$result;
+	
+	} catch(PDOException $e){
+		echo "失敗:" . $e->getMessage() . "\n";
+		exit();
+	}
+
+
 ?>
 <link rel="stylesheet" href="../css/home.css">
 <link rel="stylesheet" href="../css/subnav.css">
@@ -22,9 +50,20 @@
 		<ul class="news-contents">
             <li>保存履歴</li>
             <div class="kekka-container">
-		        <li>
+		        
+			<li>
+			
 
-                </li>
+<?php foreach($goal as $colum): ?>
+        <tr>
+			<td><?php echo $colum['id']?></td>
+            <td><?php echo $colum['data']?></td>
+			<td><?php echo $colum['code']?></td>
+        </tr>
+        <hr>
+        <?php endforeach; ?>
+
+            </li>
             </div>
 		</ul>
 </div>
