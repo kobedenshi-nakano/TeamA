ea36af228a1d3d9c1961838868cc4743852c1db5
kennag <kd1306640@st.kobedemshi.ac.jp>
2022-12-02 10:10:28 +0900

    Co-authored-by: KasaiYuuto <KasaiYuuto@users.noreply.github.com> Co-authored-by: Kazu-I-denshi <Kazu-I-denshi@users.noreply.github.com>

--- mainfile/search.php
+++ mainfile/search.php
@@ -2,6 +2,7 @@
 	require_once '../classes/dbc.php';
     require_once __DIR__ .'./header.php'
 	/*require_once __DIR__ .'./subnav.php';*/
+
 ?>
 <!DOCTYPE html>
 <link rel="stylesheet" href="../css/home.css">
@@ -12,7 +13,7 @@
 		<div class="column1">
 			<ul class="subnav">
 				<li><a href="newpost.php">新着</a></li>
-				<li><a href="select.php" style="color:red">select</a></li>
+				<li><a href="select.php" >select</a></li>
 				<li><a href="update.php">update</a></li>
 				<li><a href="delete.php">delete</a></li>
 				<li><a href="insert.php">insert</a></li>
@@ -22,29 +23,31 @@
 			</ul>
 </div>
 
+
 <div class="category_bun"> 
         <p>検索ジャンルの指定</p>
-		<form action="searchlist.php" method="POST">
       	<p>カテゴリ：</p>
+		<form action="searchlist.php" method="POST">
         <select name="category_bun">
-            <option value="1">select</option>
-            <option value="2">update</option>
-            <option value="3">insert</option>
-            <option value="4">delete</option>
-            <option value="5">creatuser</option>
-            <option value="6">creattable</option>
+			<option value=""></option>
+            <option value=1>select</option>
+            <option value=2>update</option>
+            <option value=3>insert</option>
+            <option value=4>delete</option>
+            <option value=5>creatuser</option>
+            <option value=6>creattable</option>
         </select>
-		<input type="submit" value="検索">
+		<input type="submit" name = "submit1" value="検索">
+    	<p>検索したい文字の指定</p>
+		<input type="text" name="category_code" >
+		<input type="submit" name = "submit2" value="検索">
+</div>
+</div>
 </div>
 
-<div class="category_code"> 
-        <p>検索したい文字の指定</p>
-		<form action="searchcode.php" method="POST">
-	<input type="text" name="category_code" >
-		<input type="submit" value="検索">
-</div>
 
-	</div>
+
+</div>
 
 
 
@@ -52,3 +55,4 @@
 <?php
     require_once __DIR__ .'./footer.php';
 ?>
+
